d0e2665a0fa60e98b5c59402eda7a5406720f3ff
Saurabh Chhonkar <54668764+SaurabhChhonkar@users.noreply.github.com>
2020-03-01 22:17:23 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,4 +1,28 @@
+<?php
+session_start();
+if(isset($_POST['signin']))
+{
+$email=$_POST['email'];
+$password=md5($_POST['password']);
+$sql ="SELECT EmailId,Password FROM tblusers WHERE EmailId=:email and Password=:password";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':email', $email, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['login']=$_POST['email'];
+echo "<script type='text/javascript'> document.location = 'package-list.php'; </script>";
+} else{
+	
+	echo "<script>alert('Invalid Details');</script>";
 
+}
+
+}
+
+?>
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 				<div class="modal-dialog" role="document">
 					<div class="modal-content">
@@ -24,8 +48,8 @@
 										</div>
 											<p>By logging in you agree to our <a href="page.php?type=terms">Terms and Conditions</a> and <a href="page.php?type=privacy">Privacy Policy</a></p>
 									</div>
-								</div>
-							</section>
 					</div>
-				</div>
-			</div>
+			</section>
+	</div>
+</div>
+</div>
