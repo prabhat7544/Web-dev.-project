67ca15041ecc8989c935ff96c83e3df70c4920b1
shubham9604 <54668418+shubham9604@users.noreply.github.com>
2020-03-01 22:05:19 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,31 @@
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content">
+							<section>
+								<div class="modal-body modal-spa">
+									<div class="login-grids">
+										<div class="login">
+											
+											<div class="login-right">
+												<form name="signup" method="post">
+													<h3>Create your account </h3>
+					
+
+				Name<input type="text" value=""  name="fname" autocomplete="off" required=""><br>
+				Number<input type="text" value=""  maxlength="10" name="mobilenumber" autocomplete="off" required pattern="[0-9]{10}"><br>
+		Email<input type="text" value=""  name="email" id="email" required=""><br>	
+		 <span id="user-availability-status" style="font-size:12px;"></span> 
+	password<input type="password" value=""  name="password" required=""><br>
+													<input type="submit" name="submit" id="submit" value="CREATE ACCOUNT">
+												</form>
+											</div>
+												<div class="clearfix"></div>								
+										</div>
+											<p>By logging in you agree to our <a href="page.php?type=terms">Terms and Conditions</a> and <a href="page.php?type=privacy">Privacy Policy</a></p>
+									</div>
+								</div>
+							</section>
+					</div>
+				</div>
+			</div>
