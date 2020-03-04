9ece586534e36f8d00965ad7a494c0e8deb59cb1
Saurabh Chhonkar <54668764+SaurabhChhonkar@users.noreply.github.com>
2020-03-04 06:31:15 +0530

    Update signin.php

--- include/signin.php
+++ include/signin.php
@@ -1,4 +1,28 @@
+<?php
+session_start();
+if(isset($_POST['signin']))
+{
+$email=$_POST['email'];
+$password=md5($_POST['password']);
+$sql ="SELECT EmailId,Password FROM tblusers WHERE EmailId=:email and Password=:password";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':email', $email, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['login']=$_POST['email'];
+echo "<script type='text/javascript'> document.location = 'package-list.php'; </script>";
+} else{
+	
+	echo "<script>alert('Invalid Details');</script>";
 
+}
+
+}
+
+?>
 <div class="modal fade" id="myModal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 				<div class="modal-dialog" role="document">
 					<div class="modal-content modal-info">
