c19b0242abcb1db5cad683f0f6b1987550f687b3
Saurabh Chhonkar <54668764+SaurabhChhonkar@users.noreply.github.com>
2020-03-12 21:31:06 +0530

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,46 @@
+<?php if($_SESSION['login'])
+{?>
+<div class="top-header" style="
+    color: red;
+">
+	<div class="container" >
+		<ul class="tp-hd-lft wow fadeInLeft animated" data-wow-delay=".5s" >
+			<li class="hm"><a href="index.html"><i class="fa fa-home"></i></a></li>
+			<li class="prnt"><a href="profile.php">My Profile</a></li>
+				<li class="prnt"><a href="change-password.php">Change Password</a></li>
+			<li class="prnt"><a href="tour-history.php">My Tour History</a></li>
+			<li class="prnt"><a href="issuetickets.php">Issue Tickets</a></li>
+		</ul>
+		<ul class="tp-hd-rgt wow fadeInRight animated" data-wow-delay=".5s"> 
+			<li class="tol">Welcome :</li>				
+			<li class="sig"><?php echo htmlentities($_SESSION['login']);?></li> 
+			<li class="sigi"><a href="logout.php" >/ Logout</a></li>
+        </ul>
+		<div class="clearfix"></div>
+	</div>
+</div><?php } else {?>
+<div class="top-header">
+	<div class="container">
+		<ul class="tp-hd-lft wow fadeInLeft animated" data-wow-delay=".5s">
+			<li class="hm"><a href="index.php"><i class="fa fa-home"></i></a></li>
+				<li class="hm"><a href="admin/index.php">Admin Login</a></li>
+		</ul>
+		<ul class="tp-hd-rgt wow fadeInRight animated" data-wow-delay=".5s"> 
+			<!--<li class="tol">Toll Number : 123-4568790</li>	-->			
+			<li class="sig"><a href="#" data-toggle="modal" data-target="#myModal" >Sign Up</a></li> 
+			<li class="sigi"><a href="#" data-toggle="modal" data-target="#myModal4" >/ Sign In</a></li>
+        </ul>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<?php }?>
+<!--- /top-header ---->
+<!--- header ---->
+<div class="header">
+	<div class="container">
+		<div class="logo wow fadeInDown animated" data-wow-delay=".5s">
+			<a href="index.php" style="
+    color:#ff0000;
+    font-family: bebas;
+    
+ 
