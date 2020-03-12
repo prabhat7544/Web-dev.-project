4f415ab1f32b62b8f29ab52c997154202beb8617
shubham9604 <54668418+shubham9604@users.noreply.github.com>
2020-03-12 21:34:17 +0530

    Update header.php

--- include/header.php
+++ include/header.php
@@ -42,5 +42,64 @@
 			<a href="index.php" style="
     color:#ff0000;
     font-family: bebas;
+						   
+">SSP  <span   style="
+    color:#f8ef22;
+    font-family: bebas;
+    
+ 
+" >TOURS AND TRAVELS</span></a>	
+		</div>
+	
+		<div class="lock fadeInDown animated" data-wow-delay=".5s"> 
+			<!--<li><i class="fa fa-lock"></i></li>
+            <li><div class="securetxt">SAFE &amp; SECURE </div></li>-->
+			<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!--- /header ---->
+<!--- footer-btm ---->
+<div class="footer-btm wow fadeInLeft animated" data-wow-delay=".5s">
+	<div class="container">
+	<div class="navigation">
+			<nav class="navbar navbar-default">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+					<nav class="cl-effect-1">
+						<ul class="nav navbar-nav">
+							<li><a href="index.php">Home</a></li>
+							<li><a href="page.php?type=aboutus">About</a></li>
+								<li><a href="package-list.php">Tour Packages</a></li>
+								
+						
+								<li><a href="contact.php">Contact Us</a></li>
+								<?php if($_SESSION['login'])
+{?>
+								<li><a href="#" data-toggle="modal" data-target="#myModal3">Need Help? </a>  </li>
+								<?php } else { ?>
+								<li><a href="enquiry.php"> Enquiry </a>  </li>
+								<?php } ?>
+								<div class="clearfix"></div>
+
+						</ul>
+					</nav>
+				</div><!-- /.navbar-collapse -->	
+			</nav>
+		</div>
+		
+		<div class="clearfix"></div>
+	</div>
+</div>
     
  
