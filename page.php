f4f7371ac651921ff342eee6b15a1118d6092af3
shubham9604 <54668418+shubham9604@users.noreply.github.com>
2020-03-29 22:45:41 +0530

    Update page.php

--- page.php
+++ page.php
@@ -1,3 +1,35 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(isset($_POST['submit1']))
+{
+$fname=$_POST['fname'];
+$email=$_POST['email'];	
+$mobile=$_POST['mobileno'];
+$subject=$_POST['subject'];	
+$description=$_POST['description'];
+$sql="INSERT INTO  tblenquiry(FullName,EmailId,MobileNumber,Subject,Description) VALUES(:fname,:email,:mobile,:subject,:description)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':fname',$fname,PDO::PARAM_STR);
+$query->bindParam(':email',$email,PDO::PARAM_STR);
+$query->bindParam(':mobile',$mobile,PDO::PARAM_STR);
+$query->bindParam(':subject',$subject,PDO::PARAM_STR);
+$query->bindParam(':description',$description,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+$msg="Enquiry  Successfully submited";
+}
+else 
+{
+$error="Something went wrong. Please try again";
+}
+
+}
+
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -87,3 +119,17 @@ A family-owned business based in Boston, Crystal Travel was founded in 1985 by J
 		
 	</div>
 </div>
+<!--- /privacy ---->
+<!--- footer-top ---->
+<!--- /footer-top ---->
+<?php include('includes/footer.php');?>
+<!-- signup -->
+<?php include('includes/signup.php');?>			
+<!-- //signu -->
+<!-- signin -->
+<?php include('includes/signin.php');?>			
+<!-- //signin -->
+<!-- write us -->
+<?php include('includes/write-us.php');?>
+</body>
+</html>
