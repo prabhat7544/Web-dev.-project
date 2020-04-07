b98822978a7ed507c840f40c3b7fe4d7ed7068c4
Saurabh Chhonkar <54668764+SaurabhChhonkar@users.noreply.github.com>
2020-04-07 14:16:35 +0530

    Create tour-history.php

---
+++ admin/tour-history.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['login'])==0)
+	{	
+header('location:index.php');
+}
+else{
+if(isset($_REQUEST['bkid']))
+	{
+		$bid=intval($_GET['bkid']);
+$email=$_SESSION['login'];
+	$sql ="SELECT FromDate FROM tblbooking WHERE UserEmail=:email and BookingId=:bid";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':email', $email, PDO::PARAM_STR);
+$query-> bindParam(':bid', $bid, PDO::PARAM_STR);
+$query-> execute();
+$results = $query -> fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{
+	 $fdate=$result->FromDate;
+
+	$a=explode("/",$fdate);
+	$val=array_reverse($a);
+	 $mydate =implode("/",$val);
+	$cdate=date('Y/m/d');
+	$date1=date_create("$cdate");
+	$date2=date_create("$fdate");
+ $diff=date_diff($date1,$date2);
+echo $df=$diff->format("%a");
+if($df>1)
+{
+$status=2;
+$cancelby='u';
+$sql = "UPDATE tblbooking SET status=:status,CancelledBy=:cancelby WHERE UserEmail=:email and BookingId=:bid";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':status',$status, PDO::PARAM_STR);
+$query -> bindParam(':cancelby',$cancelby , PDO::PARAM_STR);
+$query-> bindParam(':email',$email, PDO::PARAM_STR);
+$query-> bindParam(':bid',$bid, PDO::PARAM_STR);
+$query -> execute();
+
+$msg="Booking Cancelled successfully";
+}
+else
+{
+$error="You can't cancel booking before 24 hours";
+}
+}
+}
+}
+
+?>
