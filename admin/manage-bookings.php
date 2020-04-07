61bf84d8ef0515904cde94a2eb2fde2c61803792
Saurabh Chhonkar <54668764+SaurabhChhonkar@users.noreply.github.com>
2020-04-07 22:09:41 +0530

    Create manage-bookings.php

---
+++ admin/manage-bookings.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['alogin'])==0)
+	{	
+header('location:index.php');
+}
+else{ 
+	// code for cancel
+if(isset($_REQUEST['bkid']))
+	{
+$bid=intval($_GET['bkid']);
+$status=2;
+$cancelby='a';
+$sql = "UPDATE tblbooking SET status=:status,CancelledBy=:cancelby WHERE  BookingId=:bid";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':status',$status, PDO::PARAM_STR);
+$query -> bindParam(':cancelby',$cancelby , PDO::PARAM_STR);
+$query-> bindParam(':bid',$bid, PDO::PARAM_STR);
+$query -> execute();
+
+$msg="Booking Cancelled successfully";
+}
+
+
+if(isset($_REQUEST['bckid']))
+	{
+$bcid=intval($_GET['bckid']);
+$status=1;
+$cancelby='a';
+$sql = "UPDATE tblbooking SET status=:status WHERE BookingId=:bcid";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':status',$status, PDO::PARAM_STR);
+$query-> bindParam(':bcid',$bcid, PDO::PARAM_STR);
+$query -> execute();
+$msg="Booking Confirm Successfully";
+}
+
+
+
+
+	?>
+<!DOCTYPE HTML>
+<html style="
+    background-color: #333;
+">
+<head>
+<title>SSP | Admin Manage Bookings</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link rel="stylesheet" href="css/morris.css" type="text/css"/>
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<script src="js/jquery-2.1.4.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/table-style.css" />
+<link rel="stylesheet" type="text/css" href="css/basictable.css" />
+<script type="text/javascript" src="js/jquery.basictable.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+      $('#table').basictable();
+
+      $('#table-breakpoint').basictable({
+        breakpoint: 768
+      });
+
+      $('#table-swap-axis').basictable({
+        swapAxis: true
+      });
+
+      $('#table-force-off').basictable({
+        forceResponsive: false
+      });
+
+      $('#table-no-resize').basictable({
+        noResize: true
+      });
+
+      $('#table-two-axis').basictable();
+
+      $('#table-max-height').basictable({
+        tableWrapper: true
+      });
+    });
+</script>
