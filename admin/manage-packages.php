48a5413b38062d5c64276f1fa66db5b0fa90f32c
shubham9604 <54668418+shubham9604@users.noreply.github.com>
2020-04-08 10:22:59 +0530

    Update manage-packages.php

--- admin/manage-packages.php
+++ admin/manage-packages.php
@@ -58,3 +58,129 @@ else{
       });
     });
 </script>
+<!-- //tables -->
+<link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css'/>
+<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<!-- lined-icons -->
+<link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+<!-- //lined-icons -->
+</head> 
+<body>
+   <div class="page-container">
+   <!--/content-inner-->
+<div class="left-content" style="
+    background-color: #333;
+">
+	   <div class="mother-grid-inner">
+            <!--header start here-->
+				<?php include('includes/header.php');?>
+				     <div class="clearfix"> </div>	
+				</div>
+
+<div class="agile-grids">	
+				<!-- tables -->
+				
+				<div class="agile-tables">
+					<div class="w3l-table-info">
+					  <h2>Manage Existing Packages</h2>
+					    <table id="table">
+						<thead>
+						  <tr>
+						  <th>#</th>
+							<th >Name</th>
+							<th>Type</th>
+							<th>Location</th>
+							<th>Price</th>
+							<th>Creation Date</th>
+							<th>Action</th>
+						  </tr>
+						</thead>
+						<tbody>
+<?php $sql = "SELECT * from TblTourPackages";
+$query = $dbh -> prepare($sql);
+//$query -> bindParam(':city', $city, PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{				?>		
+						  <tr>
+							<td><?php echo htmlentities($cnt);?></td>
+							<td><?php echo htmlentities($result->PackageName);?></td>
+							<td><?php echo htmlentities($result->PackageType);?></td>
+							<td><?php echo htmlentities($result->PackageLocation);?></td>
+							<td>$<?php echo htmlentities($result->PackagePrice);?></td>
+							<td><?php echo htmlentities($result->Creationdate);?></td>
+							<td><a href="update-package.php?pid=<?php echo htmlentities($result->PackageId);?>"><button type="button" class="btn btn-primary btn-block">View Details</button></a></td>
+						  </tr>
+						 <?php $cnt=$cnt+1;} }?>
+						</tbody>
+					  </table>
+					</div>
+				  </table>
+
+				
+			</div>
+<!-- script-for sticky-nav -->
+		<script>
+		$(document).ready(function() {
+			 var navoffeset=$(".header-main").offset().top;
+			 $(window).scroll(function(){
+				var scrollpos=$(window).scrollTop(); 
+				if(scrollpos >=navoffeset){
+					$(".header-main").addClass("fixed");
+				}else{
+					$(".header-main").removeClass("fixed");
+				}
+			 });
+			 
+		});
+		</script>
+		<!-- /script-for sticky-nav -->
+<!--inner block start here-->
+<div class="inner-block">
+
+</div>
+<!--inner block end here-->
+<!--copy rights start here-->
+<?php include('includes/footer.php');?>
+<!--COPY rights end here-->
+</div>
+</div>
+  <!--//content-inner-->
+		<!--/sidebar-menu-->
+						<?php include('includes/sidebarmenu.php');?>
+							  <div class="clearfix"></div>		
+							</div>
+							<script>
+							var toggle = true;
+										
+							$(".sidebar-icon").click(function() {                
+							  if (toggle)
+							  {
+								$(".page-container").addClass("sidebar-collapsed").removeClass("sidebar-collapsed-back");
+								$("#menu span").css({"position":"absolute"});
+							  }
+							  else
+							  {
+								$(".page-container").removeClass("sidebar-collapsed").addClass("sidebar-collapsed-back");
+								setTimeout(function() {
+								  $("#menu span").css({"position":"relative"});
+								}, 400);
+							  }
+											
+											toggle = !toggle;
+										});
+							</script>
+<!--js -->
+<script src="js/jquery.nicescroll.js"></script>
+<script src="js/scripts.js"></script>
+<!-- Bootstrap Core JavaScript -->
+   <script src="js/bootstrap.min.js"></script>
+   <!-- /Bootstrap Core JavaScript -->	   
+
+</body>
+</html>
+<?php } ?>
