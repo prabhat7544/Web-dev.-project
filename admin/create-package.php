5f9a6c73311fbaebb11a54085b7c5c8b3503bf28
prabhat kumar <54668476+prabhat7544@users.noreply.github.com>
2020-04-09 22:16:02 +0530

    Create create-package.php

---
+++ admin/create-package.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['alogin'])==0)
+	{	
+header('location:index.php');
+}
+else{
+if(isset($_POST['submit']))
+{
+$pname=$_POST['packagename'];
+$ptype=$_POST['packagetype'];	
+$plocation=$_POST['packagelocation'];
+$pprice=$_POST['packageprice'];	
+$pfeatures=$_POST['packagefeatures'];
+$photel=$_POST['packagehotel'];
+$photels=$_POST['packagehotels'];
+$pdetails=$_POST['packagedetails'];	
+$pimage=$_FILES["packageimage"]["name"];
+move_uploaded_file($_FILES["packageimage"]["tmp_name"],"pacakgeimages/".$_FILES["packageimage"]["name"]);
+$sql="INSERT INTO TblTourPackages(PackageName,PackageType,PackageLocation,PackagePrice,PackageFetures,PackageHotel,PackageHotels,PackageDetails,PackageImage) VALUES(:pname,:ptype,:plocation,:pprice,:pfeatures,:photel,:photels,:pdetails,:pimage)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':pname',$pname,PDO::PARAM_STR);
+$query->bindParam(':ptype',$ptype,PDO::PARAM_STR);
+$query->bindParam(':plocation',$plocation,PDO::PARAM_STR);
+$query->bindParam(':pprice',$pprice,PDO::PARAM_STR);
+$query->bindParam(':pfeatures',$pfeatures,PDO::PARAM_STR);
+$query->bindParam(':photel',$photel,PDO::PARAM_STR);
+$query->bindParam(':photels',$photels,PDO::PARAM_STR);
+$query->bindParam(':pdetails',$pdetails,PDO::PARAM_STR);
+$query->bindParam(':pimage',$pimage,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+$msg="Package Created Successfully";
+}
+else 
+{
+$error="Something went wrong. Please try again";
+}
+
+}
+
+	?>
